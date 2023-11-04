6bbf5d8533239924ffd43572efea9325ec7ea931
KoshekNj <tea.zupanovic@gmail.com>
2023-11-04 22:15:39 +0100

    redirects to signup if no user is logged in

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,8 @@ import type { AppProps } from "next/app";
 import { DndContext } from "@dnd-kit/core";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import React from "react";
+import { useRouter } from "next/router";
 
 export const ItemTypes = {
   STICKER: "sticker",
@@ -11,6 +13,15 @@ export const ItemTypes = {
 const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  React.useEffect(() => {
+    if (router.asPath !== "/login" && router.asPath !== "/signup") {
+      if (localStorage.id === undefined) {
+        router.push("/signup");
+      }
+    }
+  }, [router.asPath]);
+
   return (
     <QueryClientProvider client={queryClient}>
       <Component {...pageProps} />
